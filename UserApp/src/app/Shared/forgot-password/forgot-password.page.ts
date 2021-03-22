5205059e2508a923bfb7e3b650a96ec8d30a90fa
Satish Kata -X (satkata - TECH MAHINDRA LIM at Cisco) <satkata@partnet.cisco.com>
2021-03-22 10:52:13 +0530

    login/signup/forget designs

--- UserApp/src/app/Shared/forgot-password/forgot-password.page.ts
+++ UserApp/src/app/Shared/forgot-password/forgot-password.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.page.scss'],
 })
 export class ForgotPasswordPage implements OnInit {
-
-  constructor() { }
+ userName:boolean;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+        this.userName = JSON.parse(params.userName);
+      }
+    );
   }
-
 }
